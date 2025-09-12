94ac0ea751d7727e73ed4a4770598a55664aeb2b
Asier01 <92115060+Asier01@users.noreply.github.com>
2025-09-12 18:53:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='chess_gym',
-      version='0.0.5',
-      description='OpenAI Gym environment for Chess, using the game engine of the python-chess module',
+setup(name='chess_gym_v2',
+      version='0.0.5.1',
+      description='OpenAI Gymnasium environment for Chess, using the game engine of the python-chess module',
       url='https://github.com/Ryan-Rudes/chess-gym',
       author='Ryan Rudes',
       author_email='ryanrudes@gmail.com',
       license='MIT License',
-      install_requires=['gym', 'python-chess', 'numpy', 'cairosvg', 'pillow'],
+      install_requires=['gymnasium', 'python-chess', 'numpy', 'cairosvg', 'pillow'],
       long_description=long_description,
       long_description_content_type="text/markdown",
 )
