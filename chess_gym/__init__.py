55fff9d11032d1e7efd9189d698b5b350847c703
l.gazpio <inigo.lopez@ehu.eus>
2025-10-01 16:38:46 +0200

    add max steps - upper bound of full 1000 moves

--- chess_gym/__init__.py
+++ chess_gym/__init__.py
@@ -3,11 +3,13 @@ from gymnasium.envs.registration import register
 register(
     id='Chess-v0',
     entry_point='chess_gym.envs:ChessEnv',
-    kwargs={'chess960': False}
+    kwargs={'chess960': False},
+    max_episode_steps=1000
 )
 
 register(
     id='Chess960-v0',
     entry_point='chess_gym.envs:ChessEnv',
-    kwargs={'chess960': True}
+    kwargs={'chess960': True},
+    max_episode_steps=1000
 )
