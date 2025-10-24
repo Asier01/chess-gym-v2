16805d1c26031773ddb9faee2904bafbd10c9815
Asier01 <92115060+Asier01@users.noreply.github.com>
2025-10-24 16:13:58 +0200

    Update chess_env.py

--- chess_gym/envs/chess_env.py
+++ chess_gym/envs/chess_env.py
@@ -146,8 +146,8 @@ class ChessEnv(gym.Env):
     def _get_legal_moves_index(self):
         legalMoveIndexList = []
         for move in MOVE_TO_INDEX:
+                print(list(self.board.legal_moves))
                 if move in list(self.board.legal_moves):
-                    print("a")
                     legalMoveIndexList.append(MOVE_TO_INDEX.get(move.uci()))
         print(legalMoveIndexList)
         return legalMoveIndexList
